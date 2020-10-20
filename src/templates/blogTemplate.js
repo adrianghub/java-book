43ce7e2ee5312b9e469bf487fd43c9fad3b70b30
Adrian <adrian_zinko@op.pl>
2020-10-20 18:47:22 +0200

    feat: display data fetched from graphql

--- src/templates/blogTemplate.js
+++ src/templates/blogTemplate.js
@@ -1,23 +1,34 @@
-import React from "react";
-import { graphql, Link } from "gatsby";
+import React from "react"
+import { graphql, Link } from "gatsby"
 
-import Layout from "../components/Layout/Layout";
+import Layout from "../components/Layout/Layout"
 
 export default function Template({ data }) {
-  const post = data.markdownRemark;
-  const { title, author, date } = post.frontmatter;
-};
+  const post = data.markdownRemark
+  const { title, author, date } = post.frontmatter
+
+  return (
+    <Layout>
+      <Link to="/">Back to all blogs</Link>
+      <h1>{title}</h1>
+      <p>
+        Posted by {author} on {date}
+      </p>
+      <div dangerouslySetInnerHTML={{ __html: post.html }} />
+    </Layout>
+  )
+}
 
 export const postQuery = graphql`
-query BlogPost($path: String!) {
-  markdownRemark(frontmatter: {path: {eq: $path}}) {
-    frontmatter {
-      title
-      path
-      author
-      date
+  query BlogPost($path: String!) {
+    markdownRemark(frontmatter: { path: { eq: $path } }) {
+      frontmatter {
+        title
+        path
+        author
+        date
+      }
+      html
     }
-    html
   }
-}
 `
