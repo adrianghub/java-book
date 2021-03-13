9331022741a9a9901d34b02ce44d4fe93b7c38ff
Adrian <adrian_zinko@op.pl>
2021-03-13 21:38:39 +0100

    feat: add scroll to top btn

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import Header from "../Header/Header";
+import Scroll from "../Scroll/Scroll";
 import Footer from "../Footer/Footer";
 import classes from './Layout.module.scss'
 import '../../styles/style.scss'
@@ -10,6 +11,7 @@ const Layout = ({ children }) => (
   <>
     <Header />
     <main className={classes.main}>{ children }</main>
+    <Scroll showBelow={250} />
     <Footer />
   </>
 );
