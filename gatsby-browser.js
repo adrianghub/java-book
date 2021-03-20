836b65d0ceefc6248f9aebc34ed69ac6c5ef0205
Adrian <adrian_zinko@op.pl>
2021-03-20 15:44:26 +0100

    feat: add syntax highlighting

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -6,4 +6,14 @@
 
 // You can delete this file if you're not using it
 
+import React from "react"
+import { MDXProvider } from "@mdx-js/react"
+
+import CodeBlock from "./src/components/CodeBlock/CodeBlock"
+
+const component = {
+  pre: CodeBlock
+}
+export const wrapRootElement = ({ element }) => { return <MDXProvider components={component}>{element}</MDXProvider> }
+
 require("prismjs/themes/prism.css")
--- src/components/CodeBlock/CodeBlock.jsx
+++ src/components/CodeBlock/CodeBlock.jsx
@@ -1,22 +1,56 @@
-import React from 'react'
-import Highlight, {defaultProps} from 'prism-react-renderer'
+import React, { useState } from "react"
+import Highlight, { defaultProps } from "prism-react-renderer"
 
-export default ({children, className}) => {
-  const language = className.replace(/language-/, '')
+import './CodeBlock.scss'
 
-  return (
-    <Highlight {...defaultProps} code={children} language={language}>
-      {({className, style, tokens, getLineProps, getTokenProps}) => (
-        <pre className={className} style={{...style, padding: '20px'}}>
-          {tokens.map((line, i) => (
-            <div key={i} {...getLineProps({line, key: i})}>
-              {line.map((token, key) => (
-                <span key={key} {...getTokenProps({token, key})} />
-              ))}
-            </div>
-          ))}
-        </pre>
-      )}
-    </Highlight>
-  )
+
+export default (props) => {
+    const className = props.children.props.className || 'language-text'
+
+    const language = className.replace("language-", "")
+    const matches = className.match(/language-(?<lang>.*)/)
+
+    const CopyButton = props => {
+        const [text, setText] = useState("Copy")
+
+        return (
+            <button className="code-copy-button" onClick={() => {
+                navigator.clipboard.writeText(props.content)
+                setText("Copied!")
+                setTimeout(() => { setText("Copy")}, 1000)
+            }}
+            >
+                {text}
+            </button>
+        )
+    }
+
+    return (
+        <div className="gatsby-highlight">
+            <Highlight {...defaultProps} 
+                code={props.children.props.children.trim()} 
+                language={matches && matches.groups && matches.groups.lang ? matches.groups.lang : ''}
+                theme=''
+            >
+            {({ className, tokens, getLineProps, getTokenProps}) => (
+                <pre className={className}>
+                    <div className="code-header">
+                        <span className="language-name">{language}</span>
+                        <CopyButton content={props.children.props.children} />
+                    </div>
+                    <code className={className}>
+                    {tokens.map((line, i) => (
+                        <div className="code-block" key={i} {...getLineProps({line, key:i})}>
+                            <span className="line-number">{i + 1}</span>
+                            {line.map((token, key) => (
+                                <span key={key} {...getTokenProps({token, key})} />
+                            ))}
+                        </div>
+                    ))}
+                    </code>
+                </pre>
+            )}
+            </Highlight>
+        </div>
+    )
 }
